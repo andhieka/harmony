3de1451e6bed31f070ea01ddd928c5590a3ce5a3
Andhieka Putra <andhieka.putra@gmail.com>
2014-12-08 02:07:22 +0700

    API merging 0.2

--- client/HarmonyModel.js
+++ client/HarmonyModel.js
@@ -12,29 +12,48 @@ function HarmonyModel() {
 // methods before game start
 HarmonyModel.prototype.addPlayer = function(player) {
 	if (this.gameStatus != GAME_INACTIVE) {
-		throw "Cannot add player after game has started."
+		throw new Error("Cannot add player after game has started.");
 	} else {
 		this.players.push(player);
 	}
 };
 
 HarmonyModel.prototype.setKey = function(key) {
+	if (this.gameStatus != GAME_INACTIVE) {
+		throw new Error("Cannot change key after game has started.");
+	}
 	this.board.setKey(key);
 };
 
-// methods after game start
-
 HarmonyModel.prototype.startGame = function() {
+	if (this.gameStatus != GAME_INACTIVE) {
+		throw new Error("Game has already started.");
+	}
 	this.gameStatus = GAME_STARTED;
 	this.activePlayer = Math.random() * this.players.length;
 };
+
+
+// methods after game start
+HarmonyModel.prototype.isActivePlayer = function(player) {
+	// body...
+};
+
+HarmonyModel.prototype.getPlayers = function() {
+	return this.players;
+};
+
+HarmonyModel.prototype.placeCard = function(row, col, card) {
+	this.board.placeCard(row, col, card);
+};
+
 
 HarmonyModel.prototype.getBoard = function() {
 	return this.board;
 };
 
-// methods to end game
 
+// methods to end game
 HarmonyModel.prototype.stopGame = function() {
 	this.gameStatus = GAME_STOPPED;
 
