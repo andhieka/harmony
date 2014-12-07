ddff2c3e8edb992e9d121150b24fcb62b67e4b12
Andhieka Putra <andhieka.putra@gmail.com>
2014-12-08 02:42:14 +0700

    Restructure folders for Flask

---
+++ harmony.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def index():
+	return app.send_static_file('index.html')
+
+#@app.route('/api/')
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
